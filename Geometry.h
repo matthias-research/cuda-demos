f208a9dc45f2b360139839cd8abcd8a7dcd30bda
ASUSFLOW13\matth <matthi@mail.com>
2025-10-19 14:49:29 +0200

    contact plane detection

---
+++ Geometry.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Vec3.h"
+
+
+CUDA_CALLABLE inline bool getClosestPointsOnRays(const Ray& ray0, const Ray& ray1, float& t0, float& t1)
+{
+    float a = ray0.dir.magnitudeSquared();
+    float b = -ray0.dir.dot(ray1.dir);
+    float c = ray0.dir.dot(ray1.dir);
+    float d = -ray1.dir.magnitudeSquared();
+    float e = (ray1.orig - ray0.orig).dot(ray0.dir);
+    float f = (ray1.orig - ray0.orig).dot(ray1.dir);
+    float det = a * d - b * c;
+    if (det == 0.0f) // rays are parallel
+    {
+        t0 = 0.0f; // arbitrary
+        t1 = 0.0f;
+        return false;
+    }
+    det = 1.0f / det;
+    t0 = (e * d - b * f) * det;
+    t1 = (a * f - e * c) * det;
+    return true;
+}
--- RigidBodiyKernels.h
+++ RigidBodiyKernels.h
@@ -1,7 +1,13 @@
 #include "Vec.h"
+#include "Geometry.h"
 #include "CudaUtils.h"
 #include "BVH.h"
 
+struct RigidBodyParams
+{
+    float timeStep = 0.016f;
+    float contactBand = 0.01f;
+};
 struct RigidBody
 {
     Transform transform;
@@ -10,10 +16,15 @@ struct RigidBody
     Vec3 velocity;
     Vec3 angularVelocity;
 
-    Vec3 extent;
     float invMass;
     Vec3 invInertia;
-}
+
+    int firstFace;
+    int numFaces;
+
+    int firstVertex;
+    int numVertices;
+};
 
 struct RigidBodyContact
 {
@@ -40,6 +51,12 @@ struct RigidBodyDeviceData
     void free()
     {
         bodies.free();
+        vertices.free();
+        faceFirstIndex.free();
+        faceIndices.free();
+        facePlanes.free();
+        faceCenters.free();
+        faceNeighbors.free();
         contacts.free();
         bodyLowers.free();
         bodyUppers.free();
@@ -47,25 +64,44 @@ struct RigidBodyDeviceData
         clear();
     }
 
-    void getAllocationSizes() const
+    size_t getAllocationSizes() const
     {
         size_t s = 0;
-        s += bodies.getAllocationSize();
-        s += contacts.getAllocationSize();
-        s += bodyLowers.getAllocationSize();
-        s += bodyUppers.getAllocationSize();
-        s += bvh.size();
+        s += bodies.capacity * sizeof(RigidBody);
+        s += vertices.capacity * sizeof(Vec3);
+        s += faceFirstIndex.capacity * sizeof(int);
+        s += faceIndices.capacity * sizeof(int);
+        s += facePlanes.capacity * sizeof(Plane);
+        s += faceCenters.capacity * sizeof(Vec3);
+        s += faceNeighbors.capacity * sizeof(int);
+        s += contacts.capacity * sizeof(RigidBodyContact);
+        s += bodyLowers.capacity * sizeof(Vec4);
+        s += bodyUppers.capacity * sizeof(Vec4);
+        s += bvh.getAllocationSize();
         return s;
     }
+
+    RigidBodyParams params;
 
     int numBodies;
     int numContacts;
 
+    // body data
     DeviceBuffer<RigidBody> bodies;
+    DeviceBuffer<Vec3> vertices;
+    DeviceBuffer<Plane> facePlanes;
+    DeviceBuffer<Vec3> faceCenters;
+    DeviceBuffer<int> faceFirstIndex;
+    DeviceBuffer<int> faceIndices;
+    DeviceBuffer<int> faceNeighbors;
+
+    // contact data
     DeviceBuffer<RigidBodyContact> contacts;
+
+    // acceleration structures
+    BVH bvh;
     DeviceBuffer<Vec4> bodyLowers;
     DeviceBuffer<Vec4> bodyUppers;
-    BVH bvh;
 };
 
 __global__ void device_computeBodyBounds(RigidBodyDeviceData data)
@@ -76,27 +112,157 @@ __global__ void device_computeBodyBounds(RigidBodyDeviceData data)
         return;
 
     RigidBody body = data.bodies[bodyNr];
-    Bounds bounds;
-    bounds.minimum = +body.extent;
-    bounds.maximum = -body.extent;
-    bounds.transform(body.transform);
+    
+    // Compute bounds from vertices
+    Bounds3 bounds;
+    bounds.minimum = Vec3(MaxFloat, MaxFloat, MaxFloat);
+    bounds.maximum = Vec3(-MaxFloat, -MaxFloat, -MaxFloat);
+    
+    for (int i = 0; i < body.numVertices; i++)
+    {
+        Vec3 v = body.transform * data.vertices[body.firstVertex + i];
+        bounds.minimum = bounds.minimum.min(v);
+        bounds.maximum = bounds.maximum.max(v);
+    }
 
-    data.boundsLowers[bodyNr] = Vec4(bounds.minimum);
-    data.boundsUppers[bodyNr] = Vec4(bounds.maximum);
+    data.bodyLowers[bodyNr] = Vec4(bounds.minimum);
+    data.bodyUppers[bodyNr] = Vec4(bounds.maximum);
 }
 
 
-__device__ void device_createPairContactPoints(RigidBodyDeviceData data, int bodyNr0, int bodyNr1)
+__device__ void device_findFaceContactPlane(RigidBodyDeviceData data, int bodyNr0, int bodyNr1, float& minDepth, Plane& minPlane0, Plane& minPlane1)
 {
     RigidBody body0 = data.bodies[bodyNr0];
     RigidBody body1 = data.bodies[bodyNr1];
 
-    // point face contacts
+    for (int i = 0; i < body0.numFaces; i++)
+    {
+        Plane facePlane = data.facePlanes[body0.firstFace + i];
+        // working in the frame of body1
+        Plane worldPlane = body0.transform(facePlane);
+        Plane plane = body1.transformInv(worldPlane);
 
-    // edge edge contacts
+        float maxDepth = -MaxFloat;
 
+        for (int j = 0; j < body1.numVertices; j++)
+        {
+            Vec3 p = data.vertices[body1.firstVertex + j];
+            float depth = -plane.height(p);
+            maxDepth = max(maxDepth, depth);
+
+            if (maxDepth > minDepth)
+                break;
+        }
+        if (maxDepth < minDepth)
+        {
+            minDepth = maxDepth;
+            minPlane0 = worldPlane;
+            minPlane1 = worldPlane;
+            minPlane1.d -= maxDepth;
+
+            if (minDepth < -data.params.contactBand) // separation axis found
+                return;
+        }
+    }
 }
 
+__device__ void device_findEdgeContactPlane(RigidBodyDeviceData data, int bodyNr0, int bodyNr1, float& minDepth, Plane& minPlane0, Plane& minPlane1)
+{
+    const float eps = 1e-5f;
+
+    RigidBody body0 = data.bodies[bodyNr0];
+    RigidBody body1 = data.bodies[bodyNr1];
+
+    for (int faceNr0 = 0; faceNr0 < body0.numFaces; faceNr0++)
+    {
+        int start0 = data.faceFirstIndex[body0.firstFace + faceNr0];
+        int size0 = data.faceFirstIndex[body0.firstFace + faceNr0 + 1] - start0;
+
+        for (int i = 0; i < size0; i++)
+        {
+            int neighbor0 = data.faceNeighbors[start0 + i];
+            if (neighbor0 < faceNr0)
+                continue; // already processed
+
+            int id0 = data.faceIndices[start0 + i];
+            int id1 = data.faceIndices[start0 + (i + 1) % size0];
+
+            Vec3 p0 = body0.transform(data.vertices[body0.firstVertex + id0]);
+            Vec3 p1 = body0.transform(data.vertices[body0.firstVertex + id1]);
+
+            Vec3 cp0 = body0.transform(data.faceCenters[body0.firstFace + faceNr0]);
+            Vec3 cp1 = body0.transform(data.faceCenters[body0.firstFace + neighbor0]);
+
+            for (int faceNr1 = 0; faceNr1 < body1.numFaces; faceNr1++)
+            {
+                int start1 = data.faceFirstIndex[body1.firstFace + faceNr1];
+                int size1 = data.faceFirstIndex[body1.firstFace + faceNr1 + 1] - start1;
+
+                for (int j = 0; j < size1; j++)
+                {
+                    int neighbor1 = data.faceNeighbors[start1 + j];
+                    if (neighbor1 < faceNr1)
+                        continue; // already processed
+
+                    int jd0 = data.faceIndices[start1 + j];
+                    int jd1 = data.faceIndices[start1 + (j + 1) % size1];
+
+                    Vec3 q0 = body1.transform(data.vertices[body1.firstVertex + jd0]);
+                    Vec3 q1 = body1.transform(data.vertices[body1.firstVertex + jd1]);
+
+                    Vec3 cq0 = body1.transform(data.faceCenters[body1.firstFace + faceNr1]);
+                    Vec3 cq1 = body1.transform(data.faceCenters[body1.firstFace + neighbor1]);
+
+                    Vec3 n = (p1 - p0).cross(q1 - q0).normalized();
+
+                    if (n.dot(p0 - cp0) < 0.0f) 
+                        n = -n;
+                        
+                    float depth = n.dot(p0 - q0);
+
+                    if (depth >= minDepth)
+                        continue;
+
+                    if (n.dot(p1 - cp1) < -eps)
+                        continue;
+
+                    if (n.dot(q0 - cq0) > eps)
+                        continue;
+
+                    if (n.dot(q1 - cq1) < -eps)
+                        continue;
+
+                    // float pt, qt;
+
+                    // if (!getClosestPointsOnRays(Ray(p0, p1 - p0), Ray(q0, q1 - q0), pt, qt))
+                    //     continue;
+
+                    // if (pt < -eps || pt > 1.0f + eps || qt < -eps || qt > 1.0f + eps)
+                    //     continue;
+
+                    if (depth < minDepth)
+                    {
+                        minDepth = depth;
+                        minPlane0 = Plane(n, n.dot(p0));
+                        minPlane1 = Plane(n, n.dot(q0));
+                    }
+                }
+            }
+        }
+    }
+}
+
+__device__ void device_createPairContactPoints(RigidBodyDeviceData data, int bodyNr0, int bodyNr1)
+{
+    float minDepth = MaxFloat;
+    Plane minPlane0;
+    Plane minPlane1;
+
+    device_findFaceContactPlane(data, bodyNr0, bodyNr1, minDepth, minPlane0, minPlane1);
+    device_findFaceContactPlane(data, bodyNr1, bodyNr0, minDepth, minPlane1, minPlane0);
+
+    device_findEdgeContactPlane(data, bodyNr0, bodyNr1, minDepth, minPlane0, minPlane1);
+}
 
 __global__ void device_createContactPoints(RigidBodyDeviceData data)
 {
@@ -141,9 +307,3 @@ __global__ void device_createContactPoints(RigidBodyDeviceData data)
         }
     }
 }
-
-    
-
-    
-}
-
